fb308b5addd164d7f9fd0f297a0a995e235d4921
Uz11ps <uz11ps@example.com>
2025-12-18 13:37:09 +0300

    123
    123

---
+++ lib/data/models/chat_model.g.dart
@@ -0,0 +1,44 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'chat_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ChatModel _$ChatModelFromJson(Map<String, dynamic> json) => ChatModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      type: $enumDecode(_$ChatTypeEnumMap, json['type']),
+      participantIds: (json['participantIds'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList(),
+      createdAt: DateTime.parse(json['createdAt'] as String),
+      inviteCode: json['inviteCode'] as String?,
+      lastMessageAt: json['lastMessageAt'] == null
+          ? null
+          : DateTime.parse(json['lastMessageAt'] as String),
+      lastMessage: json['lastMessage'] as String?,
+      unreadCount: (json['unreadCount'] as num?)?.toInt(),
+      likesCount: (json['likesCount'] as num?)?.toInt(),
+      meetingsCount: (json['meetingsCount'] as num?)?.toInt(),
+    );
+
+Map<String, dynamic> _$ChatModelToJson(ChatModel instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'type': _$ChatTypeEnumMap[instance.type]!,
+      'participantIds': instance.participantIds,
+      'inviteCode': instance.inviteCode,
+      'createdAt': instance.createdAt.toIso8601String(),
+      'lastMessageAt': instance.lastMessageAt?.toIso8601String(),
+      'lastMessage': instance.lastMessage,
+      'unreadCount': instance.unreadCount,
+      'likesCount': instance.likesCount,
+      'meetingsCount': instance.meetingsCount,
+    };
+
+const _$ChatTypeEnumMap = {
+  ChatType.direct: 'direct',
+  ChatType.group: 'group',
+};
---
+++ lib/data/models/user_model.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserModel _$UserModelFromJson(Map<String, dynamic> json) => UserModel(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      phone: json['phone'] as String?,
+      name: json['name'] as String?,
+      nickname: json['nickname'] as String?,
+      about: json['about'] as String?,
+      birthday: json['birthday'] == null
+          ? null
+          : DateTime.parse(json['birthday'] as String),
+      avatarUrl: json['avatarUrl'] as String?,
+    );
+
+Map<String, dynamic> _$UserModelToJson(UserModel instance) => <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'phone': instance.phone,
+      'name': instance.name,
+      'nickname': instance.nickname,
+      'about': instance.about,
+      'birthday': instance.birthday?.toIso8601String(),
+      'avatarUrl': instance.avatarUrl,
+    };
